e007a08c14a761124427cd2a9da59eda4e091a7e
Artturi Hämäläinen <artturi_hamalainen@hotmail.com>
2019-10-27 20:11:27 +0200

    added CountCharacters

---
+++ CodeWarsC/Challenges/CountCharacters.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CodeWarsC.Challenges
+{
+    /*
+    Challenge made by riston
+    The main idea is to count all the occuring characters(UTF-8) in string. If you have
+    string like this aba then the result should be { 'a': 2, 'b': 1 }
+    What if the string is empty ? Then the result should be empty object literal { }
+    For C#: Use a Dictionary<char, int> for this kata!
+    
+    Dictionary<char,int> result = CountCharacters
+        .Count1("aba");
+        foreach(KeyValuePair<char,int> d in result)
+        {
+            Console.WriteLine(d);
+                
+        }
+    */
+    class CountCharacters
+    {
+        //first dirty impelementation
+        public static Dictionary<char, int> Count(string str)
+        {
+            Dictionary<char, int> charCount = new Dictionary<char, int>();
+
+            foreach(char c in str)
+            {
+                if(charCount.ContainsKey(c))
+                {
+                    charCount[c]++;
+                } else
+                {
+                    charCount.Add(c, 1);
+                }
+            }
+
+            return charCount;
+        }
+        //with linq
+        public static Dictionary<char, int> Count1(string str)
+        {
+            return str.GroupBy(c => c).ToDictionary(g => g.Key, g => g.Count());
+        }
+    }
+}
--- CodeWarsC/Program.cs
+++ CodeWarsC/Program.cs
@@ -1,14 +1,20 @@
 ﻿using System;
+using System.Collections.Generic;
 using CodeWarsC.Challenges;
-namespace CodeWarsC
+namespace CodeWarsC.Challenges
 {
     class Program
     {
         static void Main(string[] args)
         {
 
-            int result =  SumMul35.findSum2(10);
-            System.Console.WriteLine(result);
+            Dictionary<char,int> result = CountCharacters
+            .Count1("aba");
+            foreach(KeyValuePair<char,int> d in result)
+            {
+                Console.WriteLine(d);
+                
+            }
             Console.ReadLine();
         }
     }
